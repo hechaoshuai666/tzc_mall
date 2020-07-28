699161fa1c46f9637024d46fc065d547901f008f
William <1163494766@qq.com>
2020-07-28 16:47:03 +0800

    手机验证码后端逻辑

--- tzc_mall/tzc_mall/apps/verifications/views.py
+++ tzc_mall/tzc_mall/apps/verifications/views.py
@@ -1,9 +1,16 @@
+import logging
+import random
+
 from django.views import View
 from django import http
 from django_redis import get_redis_connection
 
+from tzc_mall.utils.response_code import RETCODE
 from verifications.libs.captcha.captcha import captcha
 from . import constants
+from .libs.yuntongxun.ccp_sms import CCP
+
+logger = logging.getLogger('django')
 
 class ImageCodeView(View):
     """图形验证码"""
@@ -23,3 +30,48 @@ class ImageCodeView(View):
 
         # 响应图片验证码
         return http.HttpResponse(image, content_type='image/jpg')
+
+class SMSCodeView(View):
+    """短信验证码"""
+
+    def get(self, reqeust, mobile):
+        """
+        :param reqeust: 请求对象
+        :param mobile: 手机号
+        :return: JSON
+        """
+        # 接收参数
+        image_code_client = reqeust.GET.get('image_code')
+        uuid = reqeust.GET.get('uuid')
+
+        # 校验参数
+        if not all([image_code_client, uuid]):
+            return http.JsonResponse({'code': RETCODE.NECESSARYPARAMERR, 'errmsg': '缺少必传参数'})
+
+        # 创建连接到redis的对象
+        redis_conn = get_redis_connection('verify_code')
+        # 提取图形验证码
+        image_code_server = redis_conn.get('img_%s' % uuid)
+        if image_code_server is None:
+            # 图形验证码过期或者不存在
+            return http.JsonResponse({'code': RETCODE.IMAGECODEERR, 'errmsg': '图形验证码失效'})
+        # 删除图形验证码，避免恶意测试图形验证码
+        try:
+            redis_conn.delete('img_%s' % uuid)
+        except Exception as e:
+            logger.error(e)
+        # 对比图形验证码
+        image_code_server = image_code_server.decode()  # bytes转字符串
+        if image_code_client.lower() != image_code_server.lower():  # 转小写后比较
+            return http.JsonResponse({'code': RETCODE.IMAGECODEERR, 'errmsg': '输入图形验证码有误'})
+
+        # 生成短信验证码：生成6位数验证码
+        sms_code = '%06d' % random.randint(0, 999999)
+        logger.info(sms_code)
+        # 保存短信验证码
+        redis_conn.setex('sms_%s' % mobile, constants.SMS_CODE_REDIS_EXPIRES, sms_code)
+        # 发送短信验证码
+        CCP().send_template_sms(mobile,[sms_code, constants.SMS_CODE_REDIS_EXPIRES // 60], constants.SEND_SMS_TEMPLATE_ID)
+
+        # 响应结果
+        return http.JsonResponse({'code': RETCODE.OK, 'errmsg': '发送短信成功'})
