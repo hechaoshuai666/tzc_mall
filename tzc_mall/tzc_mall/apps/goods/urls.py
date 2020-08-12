3f2d895b33ab54d5be28c460a35c254cd296a784
William <1163494766@qq.com>
2020-08-12 11:23:38 +0800

    商品详情页后端逻辑

--- tzc_mall/tzc_mall/apps/goods/urls.py
+++ tzc_mall/tzc_mall/apps/goods/urls.py
@@ -8,8 +8,10 @@ from . import views
 
 urlpatterns = [
     # 商品首页
-    url(r'^list/(?P<category_id>\d+)/(?P<page_num>\d+)/',views.ListView.as_view(),name='list'),
+    url(r'^list/(?P<category_id>\d+)/(?P<page_num>\d+)/', views.ListView.as_view(), name='list'),
     # 热销排行
-    url(r'^hot/(?P<category_id>\d+)/',views.HotView.as_view())
+    url(r'^hot/(?P<category_id>\d+)/', views.HotView.as_view()),
+    # 商品详情
+    url(r'^detail/(?P<sku_id>\d+)/', views.DetailView.as_view())
 
 ]
--- tzc_mall/tzc_mall/apps/goods/views.py
+++ tzc_mall/tzc_mall/apps/goods/views.py
@@ -89,3 +89,28 @@ class ListView(View):
             'category_id':category_id
         }
         return render(request,'list.html',context)
+
+
+class DetailView(View):
+    """商品详情页"""
+
+    def get(self, request, sku_id):
+        """提供商品详情页"""
+        # 获取当前sku的信息
+        try:
+            sku = SKU.objects.get(id=sku_id)
+        except SKU.DoesNotExist:
+            return render(request, '404.html')
+
+        # 查询商品频道分类
+        categories = get_categories()
+        # 查询面包屑导航
+        breadcrumb = get_breadcrumb(sku.category)
+
+        # 渲染页面
+        context = {
+            'categories':categories,
+            'breadcrumb':breadcrumb,
+            'sku':sku,
+        }
+        return render(request, 'detail.html', context)
