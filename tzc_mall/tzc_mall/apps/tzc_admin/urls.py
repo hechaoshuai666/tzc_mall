dcd30731d125ce4e61ada13e4c7803ebe622d76e
William <1163494766@qq.com>
2020-08-19 22:55:18 +0800

    用户总量统计

--- tzc_mall/tzc_mall/apps/tzc_admin/urls.py
+++ tzc_mall/tzc_mall/apps/tzc_admin/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
 from rest_framework_jwt.views import obtain_jwt_token
+from .views import statistical
 
 urlpatterns = [
+    # 认证用户
     url(r'^authorizations',obtain_jwt_token),
+    # 用户总数
+    url(r'^statistical/total_count',statistical.UserTotalCountView.as_view()),
 ]
---
+++ tzc_mall/tzc_mall/apps/tzc_admin/views/statistical.py
@@ -0,0 +1,27 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author  : He Qiang
+from datetime import date
+
+
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from users.models import User
+class UserTotalCountView(APIView):
+    # 仅后台用户访问
+
+    permission_classes = (IsAdminUser,)
+
+    def get(self,request):
+        # 获取当天日期
+        now_date = date.today()
+
+        count = User.objects.count()
+
+        return Response({
+            'count':count,
+            'data':now_date
+        })
+
